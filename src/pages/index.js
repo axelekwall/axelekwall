1173da0056eca7ad5d71ab98226d599801d529bb
Axel Ekwall <axel@minimize.se>
2018-12-03 17:55:43 +0100

    Update home page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
-// import Layout from '../components/layout';
-// import Image from '../components/image';
-import Navigation from '../components/Navigation';
-import { root, content, titleRow, bioRow } from './index.module.css';
+import styles from './index.module.css';
 
 const IndexPage = () => (
-  <div className={root}>
+  <div className={styles.root}>
     <Helmet title="axel_ekwall">
       <html lang="en" />
     </Helmet>
-    <Navigation />
-    <div className={content}>
-      <div className={titleRow}>
+    <div className={styles.content}>
+      <div className={styles.textRow}>
         <h1 className={'big_title'}>Hello,</h1>
       </div>
-      <div className={bioRow}>
+      <div className={styles.textRow}>
         <p className={'front_page_bio'}>
           My name is <b>Axel Ekwall</b>. I am a media technology <b>student</b>{' '}
           at KTH Royal Institute of Technology in Stockholm, working as a{' '}
