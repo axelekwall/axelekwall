3bdd8c4d51d3662a24777d4482d338681fb59669
Axel Ekwall <axel@minimize.se>
2018-12-11 23:21:13 +0100

    Refactor style in about page

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,47 +1,47 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
-import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80vw;
+  max-width: ${({ theme }) => theme.contentWidth};
+  justify-content: flex-start;
+  margin-top: 20vh;
+  ${({ theme }) => theme.media.phone} {
+    margin-top: 10vh;
+  }
+`;
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.main};
+`;
+
+const TextRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-self: flex-start;
+`;
 
 const About = ({ data: { markdownRemark } }) => (
-  <div
-    css={theme => css`
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      width: 100vw;
-      min-height: 100vh;
-      background-color: ${theme.colors.main};
-    `}
-  >
+  <Page>
     <Helmet title={markdownRemark.frontmatter.title}>
       <html lang="en" />
     </Helmet>
-    <div
-      css={theme => css`
-        display: flex;
-        flex-direction: column;
-        width: 80vw;
-        max-width: ${theme.contentWidth};
-        justify-content: flex-start;
-        margin-top: 20vh;
-        ${theme.media.phone} {
-          margin-top: 10vh;
-        }
-      `}
-    >
-      <div
-        css={css`
-          display: flex;
-          flex-direction: row;
-          width: 100%;
-          align-self: flex-start;
-        `}
-      >
+    <ContentWrapper>
+      <TextRow>
         <h1 className={'big_title'}>{markdownRemark.frontmatter.title}</h1>
-      </div>
-    </div>
-  </div>
+      </TextRow>
+    </ContentWrapper>
+  </Page>
 );
 
 export default About;
