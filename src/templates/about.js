a8863480a53ee46125e643ea85d2b0d8decb3a93
Axel Ekwall <axel@minimize.se>
2018-12-13 16:52:35 +0100

    About template WIP

--- src/templates/about.js
+++ src/templates/about.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from '@emotion/styled';
 
 import { ContentWrapper, Page, TextRow } from '../components/elements';
+
+const ImageRow = styled.div`
+  position: relative;
+  margin-top: 10%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  width: 100%;
+  justify-content: flex-end;
+  overflow-x: visible;
+`;
 
 const About = ({ data: { markdownRemark } }) => (
   <Page backgroundColor="main">
@@ -13,6 +26,30 @@ const About = ({ data: { markdownRemark } }) => (
       <TextRow>
         <h1 className={'big_title'}>{markdownRemark.frontmatter.title}</h1>
       </TextRow>
+      <ImageRow>
+        <Img
+          style={{
+            position: 'absolute',
+            display: 'flex',
+            width: '450px',
+            right: '50%',
+            overflow: 'visible',
+          }}
+          imgStyle={{}}
+          fixed={markdownRemark.frontmatter.image.childImageSharp.fixed}
+        />
+        <div
+          style={{
+            display: 'flex',
+            width: '50%',
+            padding: '5%',
+            selfAlign: 'flex-end',
+            zIndex: 10,
+          }}
+        >
+          <p dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
+        </div>
+      </ImageRow>
     </ContentWrapper>
   </Page>
 );
@@ -24,6 +61,13 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        image {
+          childImageSharp {
+            fixed(width: 450) {
+              ...GatsbyImageSharpFixed_withWebp_tracedSVG
+            }
+          }
+        }
       }
       html
     }
