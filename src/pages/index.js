5b44bd502b43ba9c0378c9efcc34414f3fe171b4
Axel Ekwall <axel@minimize.se>
2019-02-07 17:42:47 +0100

    update links on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,23 @@ const IndexPage = () => (
         <Link to="/about">
           <b>Axel Ekwall</b>
         </Link>
-        . I am a media technology <b>student</b> at KTH Royal Institute of
-        Technology in Stockholm, working as a <b>developer</b>. I am also doing{' '}
-        <b>photography</b> and <b>experience design</b>.
+        . I am a media technology{' '}
+        <Link to="/education">
+          <b>student</b>
+        </Link>{' '}
+        at KTH Royal Institute of Technology in Stockholm, working as a{' '}
+        <Link to="/work">
+          <b>developer</b>
+        </Link>
+        . I am also doing{' '}
+        <Link to="/photography">
+          <b>photography</b>
+        </Link>{' '}
+        and{' '}
+        <Link to="/work">
+          <b>experience design</b>
+        </Link>
+        .
       </p>
     </TitleRow>
   </Layout>
