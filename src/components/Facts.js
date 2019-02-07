822eb974778ac916744e45f61b70b0d4a973f973
Axel Ekwall <axel@minimize.se>
2019-02-07 12:12:50 +0100

    add fact component

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -1,6 +1,43 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
+import styled from '@emotion/styled';
 
-const Facts = () => <div>test</div>;
+const List = styled.ul`
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  margin-bottom: 24px;
+`;
+
+const Facts = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allFactsJson {
+          edges {
+            node {
+              name
+              value
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <List>
+          {data.allFactsJson.edges.map(({ node: { name, value } }) => {
+            return (
+              <ListItem key={name}>
+                <span>{name}</span>: <span>{value}</span>
+              </ListItem>
+            );
+          })}
+        </List>
+      </div>
+    )}
+  />
+);
 
 export default Facts;
