c6a1e99b7312bc1b823e39a86e1788bac6c05401
Axel Ekwall <axel@minimize.se>
2019-02-07 12:50:28 +0100

    updated facts component

--- src/components/Facts.js
+++ src/components/Facts.js
@@ -10,6 +10,11 @@ const ListItem = styled.li`
   margin-bottom: 24px;
 `;
 
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+`
+
 const Facts = () => (
   <StaticQuery
     query={graphql`
@@ -17,8 +22,11 @@ const Facts = () => (
         allFactsJson {
           edges {
             node {
-              name
+              key
+              before
               value
+              after
+              url
             }
           }
         }
@@ -26,14 +34,23 @@ const Facts = () => (
     `}
     render={data => (
       <div>
+        <h3>Some facts</h3>
         <List>
-          {data.allFactsJson.edges.map(({ node: { name, value } }) => {
-            return (
-              <ListItem key={name}>
-                <span>{name}</span>: <span>{value}</span>
-              </ListItem>
-            );
-          })}
+          {data.allFactsJson.edges.map(
+            ({ node: { key, before, value, after, url } }) => {
+              return (
+                <ListItem key={key}>
+                  <h5>
+                    <span>{before}</span>{' '}
+                    <Link href={url} target="_blank" rel="noopener noreferrer">
+                      <b>{value}</b>
+                    </Link>{' '}
+                    <span>{after}</span>
+                  </h5>
+                </ListItem>
+              );
+            }
+          )}
         </List>
       </div>
     )}
