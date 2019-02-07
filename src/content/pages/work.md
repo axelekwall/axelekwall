5f8b608803540d6f31a9438e2c9d093878bdce1f
Axel Ekwall <axel@minimize.se>
2019-02-07 16:09:58 +0100

    add work page

---
+++ src/content/pages/work.md
@@ -0,0 +1,7 @@
+---
+title: work
+menu: true
+template: work
+slug: /work
+---
+This is a collection of projects I have contributed to. 
---
+++ src/templates/work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+import { Page } from '../components/elements';
+
+const Work = () => (
+  <Page>
+    <Helmet title={'Work'} />
+  </Page>
+);
+
+export default Work;
