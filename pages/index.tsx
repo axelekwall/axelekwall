cf77b65032609fa9e351ec400a0656a16ca4a243
Axel Ekwall <axel.ekwall@prototyp.se>
2020-10-14 22:16:08 +0200

    wip

---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const Index: FC = () => <div>Hello</div>;
+
+export default Index;
--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-module.exports = () => ({
-  plugins: [require('autoprefixer')],
-});
+module.exports = {
+  plugins: ['autoprefixer'],
+};
