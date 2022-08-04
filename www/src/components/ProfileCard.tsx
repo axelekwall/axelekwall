6252b59c69b566c103ebcc07020b90fad93de97e
Axel Ekwall <git@axelekwall.se>
2022-08-04 14:37:12 +0200

    Make it look like the old site

--- www/src/components/ProfileCard.tsx
+++ www/src/components/ProfileCard.tsx
@@ -1,21 +1,15 @@
-import ImgOfMe from "../assets/me_image.jpg";
+import React from "react";
 
 const ProfileCard = ({ children }) => (
-  <div className="my-6 flex md:flex-row w-full flex-col-reverse">
-    <div className="md:w-1/2 max-w-lg w-full">
-      {/* Fix the image here */}
+  <div className="my-6 flex sm:flex-row w-full flex-col-reverse">
+    <div className="sm:w-1/2 max-w-lg w-full">
       <img
-        placeholder="blur"
-        priority
-        sizes="(min-width: 0px) 500px"
-        layout="responsive"
-        width="500"
-        height="500"
-        src={ImgOfMe}
+        className="w-full h-full object-cover object-center m-0"
+        src="/media/me_image.jpg"
         alt="Picture of me"
       />
     </div>
-    <div className="flex flex-row-reverse md:flex-col-reverse px-0 md:px-5 w-full">
+    <div className="flex flex-row-reverse sm:flex-col-reverse px-0 sm:px-5 w-full">
       <h3 className="font-sans">{children}</h3>
     </div>
   </div>
--- www/tailwind.config.cjs
+++ www/tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["IBM Plex Serif", ...defaultTheme.fontFamily.serif],
+        mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
